dfe5e0043baf204bdc58166e7e5c06c191aa068c
alok <alokrathore136@gmail.com>
2025-06-15 00:17:35 +0530

    added comments for better readability; now when the a tag (open) is clicked the content of the data is thrown to the home as name file_cnt (as to file content)

--- index.js
+++ index.js
@@ -15,20 +15,41 @@ app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
 
+// .get for '/' whenever the user on the default '/' page we read the files_db and show the available tasks .. mainly their title 
 app.get('/',(req,res)=>{
     fs.readdir('./files_db/',(err,files)=>{
-      const DB=files
-      res.render('home',{ file_DB : DB })
+      res.render('home',{ fileNames : files })
     })
 });
 
-// get does not have req.body like post request .... it has req.query 
 
+
+// .post for 'create' ==> when the data recieved from the user (title and content of the task) we create a new file in the files_db
+
+// get does not have req.body like post request .... it has req.query 
 app.post('/create' ,(req,res)=>{
-  fs.writeFile(`./files_db/${req.body.title.split(' ').join('')}.txt`, `${req.body.content}`,'utf8', (err)=>{
-    console.log(err)
+  if (req.body.title == false ){
+    console.log('fuckUBitch')
     res.redirect('/')
-  })
-  console.log(req.body.title)
+  } 
+  else{
+
+    fs.writeFile(`./files_db/${req.body.title.split(' ').join('')}.txt`, `${req.body.content}`,'utf8', (err)=>{
+      console.log(err)
+      res.redirect('/')
+    })
+    console.log(req.body.title)
+  }
 })
 
+// on files/:fn fn is the name of the file given which is selected by the user to oopen from the home.ejs side
+app.get('/files/:fn',(req,res)=>{
+  fs.readFile(`./files_db/${req.params.fn.substring(1,req.params.fn.length)}`,'utf8',(err,data)=>{
+    if (err) {console.log(err)}
+    else console.log(data)
+    fs.readdir('./files_db',(err,file_name)=>{
+      res.render('home',{fileNames:file_name,file_cnt:data})
+      console.log('done')
+    })
+  })
+})
